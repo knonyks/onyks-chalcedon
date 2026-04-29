f1c09a5d95e6916c9f6e3cf44fe666ab9d7797b5
Xenon02 <xenon0232@gmail.com>
2026-04-29 18:48:51 +0200

    SVN Commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,95 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::process::Command;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn svn_checkout(svn_folder_path: &str, login: &str, password: &str, url: &str) -> Result<String, String> {
+    let output = Command::new("svn")
+        .current_dir(svn_folder_path) 
+        .args([
+            "checkout", url, ".", 
+            "--username", login, 
+            "--password", password, 
+            "--trust-server-cert", 
+            "--non-interactive", 
+            "--no-auth-cache"
+        ])
+        .output()
+        .map_err(|e| format!("Błąd wykonania komendy: {}", e))?;
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    } else {
+        Err(String::from_utf8_lossy(&output.stderr).to_string())
+    }
+}
+
+#[tauri::command]
+fn svn_add_all(svn_folder_path: &str) -> Result<String, String> {
+    let output = Command::new("svn")
+        .current_dir(svn_folder_path) 
+        .args([
+            "add", "--force", "."
+        ])
+        .output()
+        .map_err(|e| format!("Błąd wykonania komendy: {}", e))?;
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    } else {
+        Err(String::from_utf8_lossy(&output.stderr).to_string())
+    }
+}
+
+#[tauri::command]
+fn svn_commit(svn_folder_path: &str, login: &str, password: &str, commit_name: &str) -> Result<String, String> {
+    let output = Command::new("svn")
+        .current_dir(svn_folder_path) 
+        .args([
+            "commit", "-m", commit_name,
+            "--username", login, 
+            "--password", password, 
+            "--trust-server-cert", 
+            "--non-interactive", 
+            "--no-auth-cache"
+        ])
+        .output()
+        .map_err(|e| format!("Błąd wykonania komendy: {}", e))?;
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    } else {
+        Err(String::from_utf8_lossy(&output.stderr).to_string())
+    }
+}
+
+#[tauri::command]
+fn svn_update(svn_folder_path: &str, login: &str, password: &str) -> Result<String, String> {
+    let output = Command::new("svn")
+        .current_dir(svn_folder_path) 
+        .args([
+            "update",
+            "--username", login, 
+            "--password", password, 
+            "--trust-server-cert", 
+            "--non-interactive", 
+            "--no-auth-cache"
+        ])
+        .output()
+        .map_err(|e| format!("Błąd wykonania komendy: {}", e))?;
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    } else {
+        Err(String::from_utf8_lossy(&output.stderr).to_string())
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet,svn_checkout,svn_add_all,svn_commit,svn_update])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
