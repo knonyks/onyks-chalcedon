f3857d1616d191303eb9e55def85b2a42a49d324
zero-jedynkowy <01110010pl@gmail.com>
2026-05-08 02:41:19 +0200

    Profiles panel

---
+++ src/components/ProfileListElement.vue
@@ -0,0 +1,38 @@
+<template>
+    <onyks-list-element size="m">
+        <div class="row">
+            <onyks-avatar shape="square" type="emoji" src="🫠" size="xl"></onyks-avatar>
+            <div class="col">
+                <h3 class="profile-name">Marek Lowca Pieczarek</h3>
+                <div class="last-time-used">Last time used: 14:22, 22.04.2026</div>
+            </div>
+        </div>
+    </onyks-list-element>
+</template>
+
+<style>
+    .row 
+    {
+        display: flex;
+        align-items: center;
+        gap: var(--spacing-lg);
+    }
+
+    .col 
+    {
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-sm);
+    }
+
+    .profile-name 
+    {
+        font-weight: bold;
+    }
+
+    .last-time-used 
+    {
+        font-size: var(--size-md);
+    }
+
+</style>
--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,9 +1,124 @@
 <script setup lang="js">
     import { ref } from 'vue';
+    import { getCurrentWindow } from '@tauri-apps/api/window';
+    import ProfileListElement from '../components/ProfileListElement.vue';
+    import { useRouter } from 'vue-router'
+
+    const is_tauri = '__TAURI_INTERNALS__' in window;
+    const router = useRouter()
+
+    const openProfile = () => 
+    {
+        router.push('/profile/manager')
+    }
+
+    const addProfile = () => 
+    {
+        console.log('add profile')
+    }
+
+    const editProfile = () => 
+    {
+        console.log('edit profile')
+    }
+
+    const removeProfile = () => 
+    {
+        console.log('remove profile')
+    }
 </script>
 
 <template>
-<div>
-    <router-link to="/profile" style="color: white">Profile</router-link>
-</div>
+    <div class="xxx">
+        <div class="col a">
+            <onyks-logo></onyks-logo>
+            <div class="row b">
+                <onyks-list>
+                    <ProfileListElement></ProfileListElement>
+                    <ProfileListElement></ProfileListElement>
+                    <ProfileListElement></ProfileListElement>
+                    <ProfileListElement></ProfileListElement>
+                    <ProfileListElement></ProfileListElement>
+                    <ProfileListElement></ProfileListElement>
+                    <ProfileListElement></ProfileListElement>
+                    <ProfileListElement></ProfileListElement>
+                    <ProfileListElement></ProfileListElement>
+                </onyks-list>
+                <div class="col c">
+                    <onyks-button background="green" @click="openProfile">Open</onyks-button>
+                    <onyks-button background="blue" @click="addProfile">Add</onyks-button>
+                    <onyks-button background="purple" @click="editProfile">Edit</onyks-button>
+                    <onyks-button background="red" @click="removeProfile">Remove</onyks-button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+    .xxx
+    {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        max-width: 800px;
+        margin: 0 auto;
+        padding: var(--spacing-lg);
+        box-sizing: border-box;
+    }
+
+    .a
+    {
+        padding: var(--spacing-lg);
+        width: 800px;
+        max-height: 600px;
+    }
+
+    .a, .b, .c
+    {
+        box-sizing: border-box;
+    }
+
+    .b
+    {
+        box-sizing: border-box;
+        flex: 1; 
+        min-height: 0;
+    }
+
+    .c
+    {
+        box-sizing: border-box;
+        width: 175px;
+    }
+
+    .row
+    {
+        display: flex;
+        flex-direction: row;
+        gap: var(--spacing-lg);
+        width: 100%;
+    }
+
+    .col
+    {
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-lg);
+        height: 100%;
+    }
+
+    onyks-logo
+    {
+        width: 200px;
+    }
+
+    onyks-list
+    {
+        flex: 1;
+        height: 100%;
+        box-sizing: border-box;
+    }
+</style>
